0365e5fa7ecdb801f87ac7c7d683c48f4f395511
tz-i-git <tzu.22@icloud.com>
2024-03-16 22:25:24 +0200

    Auto-generated commit

--- assets/previews/previews.ts
+++ assets/previews/previews.ts
@@ -1,7 +1,9 @@
 
+
 import {Type} from '@angular/core';
 
 
 export const PREVIEWS_COMPONENTS_MAP: Record<string, () => Promise<Type<unknown>>> = {
+  ['pipes/power-booster.component.ts']: () => import('../../content/examples/pipes/power-booster.component').then(c => c.PowerBoosterComponent),
 
 };
